ac0612d07d38a5e4276537fb02e6c8da245840ad
xian@oryza <code@x14n.org>
2015-12-31 17:01:19 -0700

    Sep plotting code, ffmpeg, test for life
    * Minor progress report changes
    * Fiddling w/initial conditions
    * saveVideo, ffmpeg
    * Grab color settings from orig_...
    * Periodically test if life is dead (assumes period 1 or 2?)

--- test.R
+++ test.R
@@ -1,36 +1,8 @@
-require(cgolr)
-require(animation)
-
-## plotting - lattic theme
-.ncolor <- 50
-.cols <- heat.colors(.ncolor)
-.max.z <- 1
-.at <- seq(from=0, to=.max.z, length.out=.ncolor)
-
-.theme.novpadding <- list(
-    layout.heights = list(
-        top.padding = 0,
-        main.key.padding = 0,
-        key.axis.padding = 0,
-        axis.xlab.padding = 0,
-        xlab.key.padding = 0,
-        key.sub.padding = 0,
-        bottom.padding = 0
-    ),
-    layout.widths = list(
-        left.padding = 0,
-        key.ylab.padding = 0,
-        ylab.axis.padding = 0,
-        axis.key.padding = 0,
-        right.padding = 0
-    ),
-    regions=list(
-        col=.cols
-    )
-)
+pss('plot.prep.R')
 
 set.seed(1)
-.dim <- c(800,1200)
+## output dimensions
+.dim <- c(480,640)
 ## snowflakes: diam(5,5), lives(2:3) born(3), dens(.45)
 ## trees: diam(4,5), lives(2:3) born(3), dens(.55)
 ##
@@ -38,11 +10,18 @@ set.seed(1)
 .test$lives_at <- 2:3
 .test$born_at <- 3
 .test$grid <- matrix(
-    rbinom(prod(.dim), 1, 0.35),
+    rbinom(prod(.dim), 1, 0.05),
+    #rep(c(0,1,1), length.out=prod(.dim)),
+    #0,
     nrow=.dim[1], ncol=.dim[2]
 )
+.nc <- 3
+#.test$grid[1:.nc,] <- rep(1, length.out=.dim[2]*.nc)
+#.test$grid[1:.nc,] <- 1
+#.test$grid[,1:.nc] <- 0
+#.test$grid[,1:.nc] <- rep(c(0,1,1), length.out=.dim[1]*.nc)
 .test$grow <- 1.000
-.test$decay <- 0.15
+.test$decay <- 0.1
 #.nstep <- 10
 #pdf('aa.pdf')
 #for (ii in 1:.nstep) {
@@ -53,7 +32,7 @@ set.seed(1)
 
 
 ## plotting function
-movie <- function(.nstep, .silent=T, obj) {
+movie <- function(.nstep, obj, .silent=T, .compare.at=1e2) {
     .dim <- dim(obj$grid)
     .grid <- expand.grid(y=1:.dim[1], x=1:.dim[2], z=1)
     .grid$z <- as.vector(obj$grid)
@@ -62,22 +41,35 @@ movie <- function(.nstep, .silent=T, obj) {
         colorkey=F, useRaster=T, at=.at,
         par.settings=.theme.novpadding 
     )
-    plot(.plot)
+    ## comparison
+    .comp.grid <- obj$grid
     ## first step above
     for (ii in 1:(.nstep-1)) {
-        if (!.silent) cat(sprintf('Processing:\t%2.0f%%\r',(100*ii)/.nstep))
-        #par(mar=rep(0,4), oma=rep(0,4), mai=rep(0,4))
-        #image(obj$grid, useRaster=F, xaxt='n', yaxt='n', ann=F)
-        obj$step()
-        .plot$panel.args.common$z <- as.vector(obj$grid)
+        if (!.silent) cat(sprintf('## Processing:\t%2.0f%%\t\tFrame %d of %d\r',(100*ii)/.nstep, ii, .nstep))
         plot(.plot)
+        obj$step()
+        if (ii%%.compare.at) {
+            if (identical(.comp.grid==1, obj$grid==1)) {
+                cat(sprintf('## No life change at gen %d\n', ii)) 
+                return()
+            }
+            .comp.grid <- obj$grid
+        }
+        #.nc <- 4
+        #obj$grid[,1:.nc] <- rep(c(0,1,1), length.out=.dim[1]*.nc)
+        .plot$panel.args.common$z <- as.vector(obj$grid)
     }
 }
-## animation
+
 ani.options(
-    interval = 0.1, 
-    ani.height=.dim[1], ani.width=.dim[2],
-    ani.type='png', ani.dev='png'
+    interval = 0.05, 
+    ani.height=.dim[1], ani.width=.dim[2]
 )
-my.nstep <- 1e4
-saveGIF(movie(my.nstep, .silent=F, .test), movie.name='conway.big.50col.gif')
+
+my.nstep <- 5e2
+saveVideo(
+    movie(my.nstep, .test, .silent=F), 
+    video.name='conway.source.mp4',
+    ## ffmpeg opts: https://trac.ffmpeg.org/wiki/Encode/H.264
+    other.opts='-framerate 15 -qp 0  -preset veryslow -c:v libx264 -r 30 -pix_fmt yuv420p'
+)
