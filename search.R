a8d7d6357d99b48f45d2bcf4aaeaef2cf3db55c9
xian@oryza <code@x14n.org>
2016-02-10 06:16:09 -0700

    Plumbing to audio creation, mux into video
    * Generate statistics on grid as steps are taken
    * Pass statistics to audio generation
    * Combine samples, encode in vorbis
    * Mux audio / video, cleanup

--- search.R
+++ search.R
@@ -2,60 +2,15 @@ require(cgolr)
 require(animation)
 require(grid)
 require(plyr)
+pss('helpers.R')
 
 
 .grid <- 'crosshairs'
 .dim <- c(720, 1280)
 cgolr_settings(decay=0.05)
 
-.my.text <- function(
-    .text, .num, .y, .x=0.01, .just=c(0,0),
-    .gp = gpar(fontsize=20, col="grey")
-) {
-    grid.text(paste0(.text,.num), x=.x, y=.y, just=.just, gp=.gp)
-}
 
-## plot with time-jumps
-movie_steps <- function(x, 
-    .nstep = 150, .ntimes = 5, .nframes = 50,
-    .ndead = 30, .npreamble=30
-) {
-    for(kk in 1:.npreamble) {
-        plot(levelplot(x))
-        .my.text('Rule: ', x$user_data$init_settings$rule_name, .y=0.15)
-    }
-    for (ii in 1:.ntimes) {
-        for (jj in 1:.nframes) {
-            ## plot, add age, step 
-            plot(levelplot(x))
-            .my.text('Rule: ', x$user_data$init_settings$rule_name, .y=0.15)
-            .my.text('Age: ', x$age, .y=0.115)
-            .my.text('Alive: ', sprintf('%2.3f%%', x$nalive / prod(x$dim)), .y=0.08)
-            .my.text('Born: ', x$nbirth, .y=0.045)
-            .my.text('Died: ', x$ndeath, .y=0.01)
-            ## if dead long enough, call it quits
-            if (!x$nalive) {
-                .ndead = .ndead -1
-            }
-            if (!.ndead) return()
-            x$step()
-        }
-        ## fastforward nsteps, repeat plot
-        x$steps(.nstep)
-        ## report
-        cat(sprintf('## %2d%%\r', round(100*(ii/.ntimes))))
-    }
-}
 
-.init_plot <- function(x) {
-    init_plot(x,
-        ncolor=100,
-        color.live = 'red',
-        color.dead='grey10',
-        color.ramp=c('darkslateblue', 'firebrick')
-        #color.ramp=c('darkslateblue', 'green')
-    )
-}
 
 ani.options(
     ## does interval have any effect??
@@ -69,7 +24,7 @@ ani.options(
 .rules <- names(cgolr_settings_rule_by_name())
 ## make movie for each rule
 # 
-l_ply(.rules, function(.rule) {
+l_ply(.rules[5], function(.rule) {
     cat(paste0('## Processing rule ', .rule, '\n'))
     ## get settings for this rule
     .set <- cgolr_settings_rule_by_name(.rule)
@@ -78,6 +33,7 @@ l_ply(.rules, function(.rule) {
     } else {
         .nstep = 1e3
     }
+    .nstep = 300
     ## construct
     .this <- cgolr_new(
         .dim[1], .dim[2], 
@@ -87,8 +43,9 @@ l_ply(.rules, function(.rule) {
     .init_plot(.this)
     ## advance / plot / render
     .fn=paste0('scan-', .rule, '.mp4')
+    .fn.audio <- paste0('stereo.', .rule, '.ogg')
     saveVideo(
-        movie_steps(.this, .ntimes=.nstep, .nframes=1, .nstep=0),
+        movie_steps(.this, .nstep=.nstep, .npreamble=10),
         video.name=.fn,
         ## throw away - fast
         other.opts='-hide_banner -crf 20 -preset fast -c:v libx264  -pix_fmt yuv420p'
@@ -96,6 +53,13 @@ l_ply(.rules, function(.rule) {
     ## go to dir and manually run
     .cwd <- setwd(tempdir())
     system(paste0('ffmpeg -y -framerate 10 -i Rplot%d.png -hide_banner -crf 2 -preset slow -c:v libx264  -pix_fmt yuv420p ', .fn))
-    system(sprintf('cp %s %s', .fn, .cwd))
+    ## process audio
+    system("sox --combine concat 'row.*.wav' fin.row.wav")
+    system("sox --combine concat 'col.*.wav' fin.col.wav")
+    system(paste0("sox -M fin.row.wav fin.col.wav -t wav - | ffmpeg -i pipe:0 -c:a libvorbis ", .fn.audio))
+    system(sprintf('ffmpeg -y -i %s -i %s -hide_banner -c:v copy  -c:a copy fin-%s', .fn, .fn.audio, .fn ))
+    system(sprintf('cp fin-%s %s', .fn, .cwd))
+    system('rm *.wav *.ogg')
+    ## and back
     setwd(.cwd)
 })
